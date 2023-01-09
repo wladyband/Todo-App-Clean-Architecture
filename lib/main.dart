c3404999257afe073f04c9f3b813d256db89937a
Wladimir Bandeira <tecnico100@gmail.com>
2023-01-09 15:22:01 -0300

    configurando as rotas de forma global

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:todo/injection.dart' as di;
 
 import 'package:todo/theme.dart';
-import 'package:todo/ui/presentation/signup/signup_page.dart';
+import 'package:todo/ui/presentation/routes/router.gr.dart' as r;
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   await di.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  final _appRouter = r.AppRouter();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
       title: 'Todo App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
-      home: SignUpPage(),
+
     );
   }
 }
