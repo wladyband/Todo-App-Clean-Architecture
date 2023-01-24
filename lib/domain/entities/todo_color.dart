dbfba37748ffc0a446ecb0e83d6baeafd308999a
Wladimir Bandeira <tecnico100@gmail.com>
2023-01-24 17:24:57 -0300

    alterando as cores padrões

--- lib/domain/entities/todo_color.dart
+++ lib/domain/entities/todo_color.dart
@@ -4,13 +4,13 @@ class TodoColor {
   final Color color;
 
   static const List<Color> predefinedColors = [
-    Color(0xfffafafa), // canvas
-    Color(0xfffa8072), // salmon
-    Color(0xfffedc56), // mustard
-    Color(0xffd0f0c0), // tea
-    Color(0xfffca3b7), // flamingo
-    Color(0xff997950), // tortilla
-    Color(0xfffffdd0), // cream
+    Color(0xff980404),
+    Color(0xff6dd8ea),
+    Color(0xffdeb410),
+    Color(0xff234cea),
+    Color(0xff1dc439),
+    Color(0xffa46219),
+    Color(0xfffffdd0),
   ];
 
   TodoColor({required this.color});
